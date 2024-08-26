1722ddeb93c08d9ef7acb08a12aa1a55b8ad3845
Cruceanu Andrei <cruceanuandrei10@gmail.com>
2024-08-26 23:40:07 +0300

    Links navbar

---
+++ src/lib/links.ts
@@ -0,0 +1,12 @@
+type Link = {
+  href: string;
+  label: string;
+};
+
+export const links: Link[] = [
+  { href: "/", label: "Home" },
+  { href: "recipes", label: "Recipes" },
+  { href: "blog", label: "Blog" },
+  { href: "contact", label: "Contact" },
+  { href: "aboutus", label: "About us" },
+];
