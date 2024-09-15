16128184d2bac410112eaa68219ad278a3240f49
Cruceanu Andrei <cruceanuandrei10@gmail.com>
2024-09-15 11:02:41 +0300

    CircularLoading

---
+++ src/components/CircularLoader.tsx
@@ -0,0 +1,9 @@
+const CircularLoader = () => {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div className="w-8 h-8 border-4 border-primary border-t-transparent border-solid rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default CircularLoader;
