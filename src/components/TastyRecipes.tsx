97ac42510eae9d8d36f9f295c6016972376050cc
Cruceanu Andrei <cruceanuandrei10@gmail.com>
2024-09-15 11:40:35 +0300

    TastyRecipes

--- src/components/TastyRecipes.tsx
+++ src/components/TastyRecipes.tsx
@@ -1,9 +1,30 @@
-import { recipes } from "@/lib/types";
+import { useGetPopularRecipes } from "@/api/RecipeApi";
 import CardRecipe from "./CardRecipe";
+import SkeletonCard from "./SkeletonCard";
+import { Button } from "./ui";
+import Container from "./Container";
 
 const TastyRecipes = () => {
+  const { popularRecipes, isLoading } = useGetPopularRecipes();
+
+  if (isLoading) {
+    return (
+      <Container className=" mt-10 lg:mt-28">
+        <div className="flex flex-col gap-4 justify-center items-center w-full">
+          <div className="bg-gray-300 h-5 w-2/6 rounded"></div>
+          <div className="bg-gray-300 h-3 w-3/6 rounded"></div>
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-x-4 md:gap-x-12 gap-y-6 mt-12 mb-5">
+          {[1, 2, 3].map((_, index) => (
+            <SkeletonCard key={index} />
+          ))}
+        </div>
+      </Container>
+    );
+  }
+
   return (
-    <div className="custom-container max-w-7xl mt-10 lg:mt-28">
+    <Container className=" mt-10 lg:mt-28">
       <div className="flex flex-col gap-4 justify-center items-center w-full">
         <h4 className="text-2xl lg:text-4xl font-bold">
           Simple and tasty recipes
@@ -14,11 +35,14 @@ const TastyRecipes = () => {
         </p>
       </div>
       <div className="grid grid-cols-2 md:grid-cols-3 gap-x-4 md:gap-x-12 gap-y-6  mt-12 mb-5">
-        {recipes.map((recipe, index) => (
-          <CardRecipe {...recipe} key={index} />
+        {popularRecipes?.map((recipe) => (
+          <CardRecipe {...recipe} key={recipe._id} background />
         ))}
       </div>
-    </div>
+      <div className="w-full text-center mt-10">
+        <Button size="lg">More Recipes</Button>
+      </div>
+    </Container>
   );
 };
 
